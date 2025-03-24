21f5b6726709de29d44f59e1e42229e9916fa446
majick <125657+majick@users.noreply.github.com>
2025-03-23 21:00:00 -0700

    Rebrand HelpComponent.tsx, remove Terms of Use

--- src/help/HelpComponent.tsx
+++ src/help/HelpComponent.tsx
@@ -9,7 +9,7 @@ export const HelpComponent = () => {
             <div id="title">
                 <img src="icons/logo-256.png" height="80" className="profilepic" />
                 <span id="titleText">
-                    DeArrow
+                    DeArrow-GPL
                 </span>
             </div>
 
@@ -43,7 +43,7 @@ export const HelpComponent = () => {
                     {chrome.i18n.getMessage("termsAgreement").split("{privacy-policy}")[0]}
                     <a href="https://gist.github.com/ajayyy/9bfec83d57ea34f5182658ec8445aa9d" target="_blank" rel="noreferrer">{chrome.i18n.getMessage("privacyPolicy")}</a>
                     {chrome.i18n.getMessage("termsAgreement").split("{privacy-policy}")[1].split("{terms}")[0]} 
-                    <a href="https://gist.github.com/ajayyy/9e8100f069348e0bc062641f34d6af12" target="_blank" rel="noreferrer">{chrome.i18n.getMessage("termsOfUse")}</a>.{" "}
+                    <a href="https://github.com/majick/DeArrow-GPL/blob/master/LICENSE" target="_blank" rel="noreferrer">{chrome.i18n.getMessage("termsOfUse")}</a>.{" "} -- GPL.
                 </p>
 
                 {
